b0dbb6320007f8a44c44c6bafa6eabf4d54c3ab6
Roman Lyakh <ctarewer@gmail.com>
2022-05-24 14:39:26 +0300

    end progect

--- education-task-calculator-master/src/pages/Home/components.js
+++ education-task-calculator-master/src/pages/Home/components.js
@@ -6,15 +6,32 @@ export const Card = styled.div`
 
   background-color: ${({ theme }) => theme.colors.white};
 
-  border-radius: 32px;
-
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  box-shadow: ${({ theme }) => theme.boxShadows[0]};
-
-  padding: ${({ theme }) => theme.spaces[4]}px;
 `
 
 export const Heading = styled.h3``
+
+export const CalculatorContainer = styled.div`
+  height: 70%;
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+`
+
+export const ControlPanelConteiner = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-content: flex-end;
+  margin-right: 20px;
+`
+export const Line = styled.div`
+  height: ${props => (props.vertical ? '100%' : '0px')};
+  width: ${props => (props.horizontal ? '100%' : '0px')};
+  border: 2px solid ${({ theme }) => theme.colors.darkGrey};
+  border-radius: 12px;
+  opacity: 1;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+`
